e2d83c3e37f91a64aa7e1fc1979e23e4646fde3f
Yuşa Sarısoy <yusasarisoy@icloud.com>
2021-04-18 18:31:00 +0300

    Log out via Firebase.

--- hashtalk/Controllers/ChatViewController.swift
+++ hashtalk/Controllers/ChatViewController.swift
@@ -6,19 +6,29 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        title = "Hashtalk" // Set the title as app name.
+        navigationItem.hidesBackButton = true // Hide the "Back" button.
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error while signing out %@:", signOutError)
+        }
+    }
 }
