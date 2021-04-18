8018ead21571a374fb0901c848ab63dc3f524761
Yuşa Sarısoy <yusasarisoy@icloud.com>
2021-04-18 18:18:39 +0300

    Log in via Firebase.

--- hashtalk/Controllers/LoginViewController.swift
+++ hashtalk/Controllers/LoginViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
-    @IBAction func loginPressed(_ sender: UIButton) {
-    }
     
+    @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+                if let e = error {
+                    print(e)
+                } else {
+                    self.performSegue(withIdentifier: "LogInToChat", sender: self)  // Navigate to the ChatViewController.
+                }
+            }
+        }
+    }
 }
