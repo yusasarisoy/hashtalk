441ff2d39b7bf4aa5cd5449ac96ce01cb3121f69
Yuşa Sarısoy <yusasarisoy@icloud.com>
2021-04-24 17:25:45 +0300

    Save the message to the Firestore.

--- hashtalk/Controllers/ChatViewController.swift
+++ hashtalk/Controllers/ChatViewController.swift
@@ -13,6 +13,8 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
     var messages: [Message] = [
         Message(sender: "1@2.com", body: "Hey!"),
         Message(sender: "a@b.com", body: "Hello!"),
@@ -29,6 +31,7 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        saveMessageToFirestore() // This function provides to save the message to the Firestore.
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -37,6 +40,24 @@ class ChatViewController: UIViewController {
             navigationController?.popToRootViewController(animated: true)
         } catch let signOutError as NSError {
             print("Error while signing out %@:", signOutError)
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    /// This function provides to save the message to the Firestore.
+    private func saveMessageToFirestore() {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email { // Get the message and email of message sender.
+            db.collection(Constants.Firestore.collectionName).addDocument(data: [
+                Constants.Firestore.senderField : messageSender,
+                Constants.Firestore.bodyField: messageBody
+            ]) { (error) in
+                if let e = error {
+                    print("The following error occurred while saving data to the Firestore: \(e.localizedDescription)")
+                } else {
+                    print("Data has been saved successfully!")
+                }
+            }
         }
     }
 }
